8403512f829957e3c5d114a98e70f3e5c6ec7b65
b96705008 <b96705008@gmail.com>
2015-08-19 15:25:01 +0800

    replace searchparam

--- example/controller.js
+++ example/controller.js
@@ -3,5 +3,11 @@ var Monster = require('./model');
 module.exports = function(req, res){
 	Monster.apiQuery(req.query).exec(function (err, monsters) {
 	 	res.send(monsters);
-	});
+	});	
+
+	// Monster.apiQuery(req.query, {
+	// 	findCond: {name: 'Big Purple People Eater'}
+	// }).exec(function (err, monsters) {
+	//  	res.send(monsters);
+	// });
 };
